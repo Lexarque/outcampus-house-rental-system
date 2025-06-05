41b5af064b28c32ed76854a3dc32148fc8b55bd6
Satrialdy Darrel Farizki <satrialdyfarizki06@gmail.com>
2025-06-06 00:49:09 +0800

    Just leave this first for now trust gng

--- src/auth/view/AuthenticationView.java
+++ src/auth/view/AuthenticationView.java
@@ -9,6 +9,10 @@ public class AuthenticationView {
         System.out.print("Please select an option:\n1. Register\n2. Login\n3. Exit\n: ");
         int option = Integer.parseInt(scanner.nextLine());
 
+        if (option == 3) {
+            return;
+        }
+
         System.out.print("Enter your phone number: ");
         String phone = scanner.nextLine();
         System.out.print("Enter your password: ");
@@ -20,23 +24,20 @@ public class AuthenticationView {
                 String name = scanner.nextLine();
                 System.out.print("Enter your role (landlord/tenant): ");
                 String role = scanner.nextLine();
-                User user = new User(name, phone, password, role);
-                user.register();
-                user.login(phone, password);
+//                User user = new User(name, phone, password, role);
+//                user.register();
+//                user.login(phone, password);
                 break;
             case 2:
-                User user2 = new User("", phone, password, "");
-                user2.login(phone, password);
-                if (user2.getStatus().isEmpty()) {
-                    do {
-                        String newPhone = scanner.nextLine();
-                        String newPassword = scanner.nextLine();
-                        user2.login(newPhone, newPassword);
-                    } while (user2.getStatus().isEmpty());
-                }
-                break;
-            case 3:
-                System.out.println("Exiting...");
+//                User user2 = new User("", phone, password, "");
+//                user2.login(phone, password);
+//                if (user2.getStatus().isEmpty()) {
+//                    do {
+//                        String newPhone = scanner.nextLine();
+//                        String newPassword = scanner.nextLine();
+//                        user2.login(newPhone, newPassword);
+//                    } while (user2.getStatus().isEmpty());
+//                }
                 break;
         }
     }
